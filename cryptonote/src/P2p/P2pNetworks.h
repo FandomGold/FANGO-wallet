36900fbb2446555600e92a5294d7b3e49ba2ef12
ColinRitman <aejontargaryen@pm.me>
2021-07-17 08:54:14 +0000

    fango core v1.8.0

---
+++ cryptonote/src/P2p/P2pNetworks.h
@@ -0,0 +1,12 @@
+// Copyright (c) 2011-2017 The Cryptonote developers
+// Copyright (c) 2017-2018 The Circle Foundation & Conceal Devs
+// Copyright (c) 2018-2019 Conceal Network & Conceal Devs
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+namespace CryptoNote
+{
+ const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0x46, 0x41, 0x4e, 0x44, 0x4f, 0x4d, 0x47, 0x4f, 0x4c, 0x44, 0x00, 0x12, 0x10, 0x11, 0x01, 0x10 } };
+}
